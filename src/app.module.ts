880788ca4c4718a25807496694b45132470e3f12
aayushsapkota9 <aayushsapkota9@gmail.com>
2023-12-29 01:01:13 +0545

    added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfig } from './config/postgres.config';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfig,
+      inject: [PostgresConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
